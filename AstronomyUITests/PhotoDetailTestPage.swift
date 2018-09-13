276a3890382bc9966a222c9106a214dae5d8cb53
William Bundy <williambundy@icloud.com>
2018-09-13 16:11:26 -0700

    Created tests, added test framework basics

---
+++ AstronomyUITests/PhotoDetailTestPage.swift
@@ -0,0 +1,9 @@
+//
+//  PhotoDetailTestPage.swift
+//  AstronomyUITests
+//
+//  Created by William Bundy on 9/13/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import Foundation
