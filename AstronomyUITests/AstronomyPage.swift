5c1db8974311ecae08d38b9f35719efbc13fd817
Conner Alegre <cga0@pm.me>
2018-09-13 13:42:51 -0600

    setup base AstronomyPage

---
+++ AstronomyUITests/AstronomyPage.swift
@@ -0,0 +1,19 @@
+//
+//  AstronomyPage.swift
+//  AstronomyUITests
+//
+//  Created by Conner on 9/13/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import XCTest
+
+struct AstronomyPage: TestPage {
+    let testCase: XCTestCase
+    
+    // MARK: - Elements
+    
+    // MARK: - Interactions
+    
+    // MARK: - Verifications
+}
