9e661d6970aa868270f61d5788a03d43c25c6c44
Linh Bouniol <paulalinh89@gmail.com>
2018-09-13 20:47:25 -0700

    Added UI tests

--- AstronomyUITests/AstronomyUITests.swift
+++ AstronomyUITests/AstronomyUITests.swift
@@ -16,7 +16,7 @@ class AstronomyUITests: XCTestCase {
         let app = XCUIApplication()
         
         app.launchArguments = ["UITesting"]
-        
+    
         /*
          This project is set up so we can test the UI without having to worry about the uncertainty that network calls bring into the application.
          The string of the launch argument is what the application will look for to know that it use local data instead of making network calls
@@ -25,7 +25,7 @@ class AstronomyUITests: XCTestCase {
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
+        app.launch()
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
         
@@ -39,6 +39,37 @@ class AstronomyUITests: XCTestCase {
     func testExample() {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+    }
+    
+    func testSavePhoto() {
+        
+        PhotosCollectionViewPage(testCase: self)
+            .tapOnCollectionCell(at: 0)
+            .tapOnSaveButton()
+            .verifyAlertShowsUp()
+    }
+    
+    func testNavigateToNextSol() {
+        
+        PhotosCollectionViewPage(testCase: self)
+            .tapOnNextSolButton()
+            .verifyTitle(title: "Sol 16")
+    }
+    
+    func testNavigateToPreviousSol() {
+        
+        PhotosCollectionViewPage(testCase: self)
+            .tapOnPreviousSolButton()
+            .verifyTitle(title: "Sol 14")
+    }
+    
+    func testImage() {
+        
+        PhotosCollectionViewPage(testCase: self)
+            .tapOnNextSolButton()
+            .tapOnCollectionCell(at: 0) // now in detail
+            .verifyImage()
     }
     
 }
---
+++ AstronomyUITests/PhotosCollectionViewPage.swift
@@ -0,0 +1,76 @@
+//
+//  PhotoPage.swift
+//  AstronomyUITests
+//
+//  Created by Linh Bouniol on 9/13/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import XCTest
+
+struct PhotosCollectionViewPage: TestPage {
+    
+    let testCase: XCTestCase
+    
+    // MARK: - Elements
+    
+    // PhotosCollectionViewController - previousSolButton, nextSolButton, image view, collection view cell, navigation bar title
+    
+    var previousSolButton: XCUIElement {
+        return app.buttons["PhotosCollectionViewController.PreviousSolButton"]
+    }
+    
+    var nextSolButton: XCUIElement {
+        return app.buttons["PhotosCollectionViewController.NextSolButton"]
+    }
+    
+    // There are many cells, instead of creating a computed variable for each cell at a given index, we create a function that will return a cell at any index we're taping on.
+    func collectionCell(at index: Int) -> XCUIElement {
+        return app.collectionViews.cells.element(boundBy: index)
+    }
+    
+//    func solTitle(with number: Int) -> XCUIElement {
+//        return app.navigationBars.staticTexts["Sol\(number)"]
+//    }
+    
+    func navigationBar(withTitle title: String) -> XCUIElement {
+        return app.navigationBars[title]
+    }
+    
+    // MARK: - Actions (Interactions)
+    
+    // PhotosCollectionViewController
+    
+    @discardableResult func tapOnPreviousSolButton(file: String = #file, line: UInt = #line) -> PhotosCollectionViewPage {
+        
+        // Make sure previousSolButton exists
+        testCase.expect(exists: previousSolButton, file: file, line: line)
+        previousSolButton.tap()
+        return self
+    }
+    
+    @discardableResult func tapOnNextSolButton(file: String = #file, line: UInt = #line) -> PhotosCollectionViewPage {
+        
+        testCase.expect(exists: nextSolButton, file: file, line: line)
+        nextSolButton.tap()
+        return self
+    }
+    
+    @discardableResult func tapOnCollectionCell(at index: Int, file: String = #file, line: UInt = #line) -> PhotoDetailViewPage {
+        
+        // Get cell
+        let cell = collectionCell(at: index)
+        testCase.expect(exists: cell, file: file, line: line)
+        cell.tap()
+        return PhotoDetailViewPage(testCase: testCase)
+    }
+    
+    // MARK: - Verifications
+    
+    // verify title
+    @discardableResult func verifyTitle(title: String, file: String = #file, line: UInt = #line) -> PhotosCollectionViewPage {
+        
+        testCase.expect(exists: navigationBar(withTitle: title), file: file, line: line)
+        return self
+    }
+}
