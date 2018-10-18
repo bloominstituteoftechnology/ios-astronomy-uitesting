52068f281ea4ac839d4ea6d9bf130181cb383bc1
Ilqar Ilyasov <ilqar_ilyasov@yahoo.com>
2018-10-18 15:35:28 -0500

    UITestCase+Except added

---
+++ AstronomyUITests/PhotoDetailPage.swift
@@ -0,0 +1,9 @@
+//
+//  PhotoDetailPage.swift
+//  AstronomyUITests
+//
+//  Created by Ilgar Ilyasov on 10/18/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import Foundation
---
+++ AstronomyUITests/TestPage.swift
@@ -0,0 +1,20 @@
+//
+//  TestPage.swift
+//  AstronomyUITests
+//
+//  Created by Ilgar Ilyasov on 10/18/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import Foundation
+import XCTest
+
+protocol TestPage {
+    var testCase: XCTestCase { get }
+}
+
+extension TestPage {
+    var app: XCUIApplication {
+        return XCUIApplication()
+    }
+}
