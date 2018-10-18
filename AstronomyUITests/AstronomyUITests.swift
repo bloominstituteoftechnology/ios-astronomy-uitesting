bbef505417246b2dfb832fcadf92c71381d2e4a7
Ilqar Ilyasov <ilqar_ilyasov@yahoo.com>
2018-10-18 15:45:44 -0500

    testExample recorded

--- AstronomyUITests/AstronomyUITests.swift
+++ AstronomyUITests/AstronomyUITests.swift
@@ -32,6 +32,43 @@ class AstronomyUITests: XCTestCase {
     }
 
     func testExample() {
+        
+        let app = XCUIApplication()
+        let element = app.collectionViews.children(matching: .cell).element(boundBy: 0).children(matching: .other).element
+        element.tap()
+        
+        let element3 = app.otherElements.containing(.navigationBar, identifier:"Title").children(matching: .other).element.children(matching: .other).element
+        let element2 = element3.children(matching: .other).element
+        element2.tap()
+        element2.tap()
+        element2.tap()
+        
+        let photodetailviewcontrollerSavebuttonButton = app/*@START_MENU_TOKEN@*/.buttons["PhotoDetailViewController.SaveButton"]/*[[".buttons[\"Save to Photo Library\"]",".buttons[\"PhotoDetailViewController.SaveButton\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        photodetailviewcontrollerSavebuttonButton.tap()
+        
+        let okayButton = app.alerts["Photo Saved!"].buttons["Okay"]
+        okayButton.tap()
+        
+        let titleNavigationBar = app.navigationBars["Title"]
+        titleNavigationBar.buttons["Sol 1"].tap()
+        app.navigationBars["Sol 1"]/*@START_MENU_TOKEN@*/.buttons["PhotosCollectionViewController.NextSolButton"]/*[[".buttons[\">\"]",".buttons[\"PhotosCollectionViewController.NextSolButton\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        element.tap()
+        element2.tap()
+        element2.tap()
+        photodetailviewcontrollerSavebuttonButton.tap()
+        okayButton.tap()
+        
+        let sol2Button = titleNavigationBar.buttons["Sol 2"]
+        sol2Button.tap()
+        app.otherElements.containing(.navigationBar, identifier:"Sol 2").children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .collectionView).element.swipeUp()
+        element3.children(matching: .other).element(boundBy: 1).tap()
+        element2.tap()
+        element2.tap()
+        element2.tap()
+        app.otherElements.containing(.alert, identifier:"Photo Saved!").children(matching: .other).element(boundBy: 0).tap()
+        okayButton.tap()
+        sol2Button.tap()
+        
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
