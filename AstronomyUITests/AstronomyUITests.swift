0a20f02460e0f52c9e561cc70c7d16307bd6458e
Ilqar Ilyasov <ilqar_ilyasov@yahoo.com>
2018-10-18 19:27:30 -0500

    PhotosCollectionPage completed except Verification

--- AstronomyUITests/AstronomyUITests.swift
+++ AstronomyUITests/AstronomyUITests.swift
@@ -13,7 +13,7 @@ class AstronomyUITests: XCTestCase {
     override func setUp() {
         
         let app = XCUIApplication()
-        app.launchArguments = ["UITesting"] // The App will look for to know that it use local data instead of making network calls
+        app.launchArguments = ["UITesting"] // The App will look for to know that it use local data instead of making network calls IMPORTANT !!!
         
         
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -34,40 +34,36 @@ class AstronomyUITests: XCTestCase {
     func testExample() {
         
         let app = XCUIApplication()
-        let element = app.collectionViews.children(matching: .cell).element(boundBy: 0).children(matching: .other).element
-        element.tap()
         
-        let element3 = app.otherElements.containing(.navigationBar, identifier:"Title").children(matching: .other).element.children(matching: .other).element
-        let element2 = element3.children(matching: .other).element
-        element2.tap()
-        element2.tap()
-        element2.tap()
+        // Tap on Navigation Item Title
+        XCUIApplication().navigationBars["Sol 1"].otherElements["Sol 1"].tap()
         
-        let photodetailviewcontrollerSavebuttonButton = app/*@START_MENU_TOKEN@*/.buttons["PhotoDetailViewController.SaveButton"]/*[[".buttons[\"Save to Photo Library\"]",".buttons[\"PhotoDetailViewController.SaveButton\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
-        photodetailviewcontrollerSavebuttonButton.tap()
         
-        let okayButton = app.alerts["Photo Saved!"].buttons["Okay"]
-        okayButton.tap()
+        // Tap on PhotosCollectionVC Image
+        XCUIApplication().collectionViews.children(matching: .cell).element(boundBy: 0).otherElements.containing(.image, identifier:"PhotosCollectionViewController.ImageView").element.tap()
         
-        let titleNavigationBar = app.navigationBars["Title"]
-        titleNavigationBar.buttons["Sol 1"].tap()
-        app.navigationBars["Sol 1"]/*@START_MENU_TOKEN@*/.buttons["PhotosCollectionViewController.NextSolButton"]/*[[".buttons[\">\"]",".buttons[\"PhotosCollectionViewController.NextSolButton\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-        element.tap()
-        element2.tap()
-        element2.tap()
-        photodetailviewcontrollerSavebuttonButton.tap()
-        okayButton.tap()
         
-        let sol2Button = titleNavigationBar.buttons["Sol 2"]
-        sol2Button.tap()
-        app.otherElements.containing(.navigationBar, identifier:"Sol 2").children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .collectionView).element.swipeUp()
-        element3.children(matching: .other).element(boundBy: 1).tap()
-        element2.tap()
-        element2.tap()
-        element2.tap()
-        app.otherElements.containing(.alert, identifier:"Photo Saved!").children(matching: .other).element(boundBy: 0).tap()
-        okayButton.tap()
-        sol2Button.tap()
+        // Tap on PhotoDetailVC Image
+        app.otherElements.containing(.navigationBar, identifier:"Title").children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.tap()
+        
+        // Tap on PhotoDetailVC Save to Photo Library Button
+        app/*@START_MENU_TOKEN@*/.buttons["PhotoDetailViewController.SaveButton"]/*[[".buttons[\"Save to Photo Library\"]",".buttons[\"PhotoDetailViewController.SaveButton\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        
+        // Tap on PhotoDetailVC Alert Okay Button
+        app.alerts["Photo Saved!"].buttons["Okay"].tap()
+        
+        // Tap on PhotoDetailVC Back button
+        XCUIApplication().navigationBars["Title"].buttons["Sol 1"].tap()
+        
+        // PhotosCollectionVC Next Sol
+        XCUIApplication().navigationBars["Sol 1"]/*@START_MENU_TOKEN@*/.buttons["PhotosCollectionViewController.NextSolButton"]/*[[".buttons[\">\"]",".buttons[\"PhotosCollectionViewController.NextSolButton\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        
+        // PhotosCollectionVC Previous Sol
+        XCUIApplication().navigationBars["Sol 2"]/*@START_MENU_TOKEN@*/.buttons["PhotosCollectionViewController.PreviousSolButton"]/*[[".buttons[\"<\"]",".buttons[\"PhotosCollectionViewController.PreviousSolButton\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        
+        // PhotosCollectionVC Scroll Down
+        XCUIApplication().otherElements.containing(.navigationBar, identifier:"Sol 1").children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .collectionView).element.swipeUp()
+        
         
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
--- AstronomyUITests/PhotosCollectionPage.swift
+++ AstronomyUITests/PhotosCollectionPage.swift
@@ -7,5 +7,47 @@
 //
 
 import Foundation
+import XCTest
 
+struct PhotosCollectionPage: TestPage {
+    
+    var testCase: XCTestCase
+    
+    // UI Elements
+    var previousSolButton: XCUIElement {
+        return app.navigationBars.buttons["PhotosCollectionViewController.PreviousSolButton"]
+    }
+    
+    var nextSolButton: XCUIElement {
+        return app.navigationBars.buttons["PhotosCollectionViewController.NextSolButton"]
+    }
+    
+    @discardableResult func navigationItemTitle(for index: Int) -> XCUIElement {
+        return app.navigationBars.staticTexts["Sol \(index)"]
+    }
+    
+    @discardableResult func imageView(on index: Int) -> XCUIElement {
+        return app.collectionViews.children(matching: .cell).element(boundBy: index).otherElements.containing(.image, identifier:"PhotosCollectionViewController.ImageView").firstMatch
+//        return app.collectionViews.cells.images["PhotosCollectionViewController.ImageView"]
+    }
+    
+    // Actions
+    
+    @discardableResult func tapOnLeftBarButton() -> PhotosCollectionPage {
+        previousSolButton.tap()
+        return self
+    }
+    
+    @discardableResult func tapOnRightBarButton() -> PhotosCollectionPage {
+        nextSolButton.tap()
+        return self
+    }
+    
+    @discardableResult func tapOnImageView(index: Int, file: String, line: UInt) -> PhotoDetailPage {
+        imageView(on: index)
+        return PhotoDetailPage(testCase: testCase)
+    }
+    
+    // Verifications
 
+}
