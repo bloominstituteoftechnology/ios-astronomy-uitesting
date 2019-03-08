59e021776abdd7218b7acf44c46c4f7997622a98
nateyoungren <nathanaelyoungren@gmail.com>
2019-03-07 16:11:01 -0800

    Add UI Tests for initial state, tap on image, save image, and tapping right and left sol buttons.

---
+++ AstronomyUITests/AstronomyUITests.swift
@@ -0,0 +1,99 @@
+//
+//  AstronomyUITests.swift
+//  AstronomyUITests
+//
+//  Created by Nathanael Youngren on 3/7/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import XCTest
+
+enum Sol: String {
+    case fourteen = "Sol 14"
+    case fifteen = "Sol 15"
+    case sixteen = "Sol 16"
+}
+
+enum Direction {
+    case right
+    case left
+}
+
+class AstronomyUITests: XCTestCase {
+    
+    var app: XCUIApplication!
+    
+    override func setUp() {
+
+        continueAfterFailure = false
+        app = XCUIApplication()
+        app.launchArguments = ["UITesting"] // Use local data instead of network calls
+        app.launch()
+        addUIInterruptionMonitor(withDescription: "System Dialog") {
+            (alert) -> Bool in
+            
+            if alert.buttons["Allow"].exists {
+                alert.buttons["Allow"].tap()
+                self.saveButton.tap()
+            }
+            return true
+        }
+    }
+    
+    func testInitialState() {
+        XCTAssertTrue(leftButton.exists)
+        XCTAssertTrue(rightButton.exists)
+    }
+    
+    func testTapOnImageAndGoBack() {
+        
+        for i in 0..<app.collectionViews.cells.count {
+            let cell = app.collectionViews.cells.element(boundBy: i)
+            cell.tap()
+            app.navigationBars.buttons.element(boundBy: 0).tap()
+        }
+    }
+    
+    func testSavingAnImage() {
+        app.collectionViews.cells.element(boundBy: 1).tap()
+        saveButton.tap()
+        app.alerts["Photo Saved!"].buttons["Okay"].tap()
+    }
+    
+    func testTapRightButtonViewSol() {
+        tapSolButton(going: .right)
+    }
+    
+    func testTapLeftButtonViewSol() {
+        tapSolButton(going: .left)
+    }
+    
+    func tapSolButton(going direction: Direction) {
+        switch direction {
+        case .right:
+            for _ in 0..<2 {
+                rightButton.tap()
+            }
+        case .left:
+            for _ in 0..<2 {
+                rightButton.tap()
+            }
+            for _ in 0..<2 {
+                leftButton.tap()
+            }
+        }
+    }
+    
+    var leftButton: XCUIElement {
+        return app.navigationBars.buttons["PhotosCollectionViewController.PreviousSolButton"]
+    }
+    
+    var rightButton: XCUIElement {
+        return app.navigationBars.buttons["PhotosCollectionViewController.NextSolButton"]
+    }
+    
+    var saveButton: XCUIElement {
+        return app/*@START_MENU_TOKEN@*/.buttons["PhotoDetailViewController.SaveButton"]/*[[".buttons[\"Save to Photo Library\"]",".buttons[\"PhotoDetailViewController.SaveButton\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+    }
+    
+}
