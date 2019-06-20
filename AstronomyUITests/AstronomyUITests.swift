b69be6aad65fce671a23538fe18806389a93ca19
Diante Lewis-Jolley <diantelewis-jolley@Diantes-MacBook-Pro.local>
2019-06-20 18:16:14 -0400

    added new test for cells

--- AstronomyUITests/AstronomyUITests.swift
+++ AstronomyUITests/AstronomyUITests.swift
@@ -21,8 +21,60 @@ class AstronomyUITests: XCTestCase {
         app.launchArguments = ["UITesting"]
         app.launch()
 
+    }
+
+    //Identify the UI Elements (likely everything)
+    // Add accessibility identifiers to UI Elements
+    // Add any convience methods to tap on elements
+    // ALWAYS remember to verify behavior through assertions where possible
+
+    func testingSavingPhoto() {
+
+        let collectionViews = app.collectionViews["PhotoCollectionView"]
+        let roverCell = collectionViews.cells.element(boundBy: 0)
+        let saveButton = app.buttons["PhotoDetailViewController.SaveButton"]
+        XCTAssert(saveButton.exists)
+        XCTAssertTrue(collectionViews.exists)
+        
+
+        roverCell.tap()
+        saveButton.tap()
 
 
+    }
+
+    func testNextSol() {
+
+        let nextSolButton = app.buttons["PhotosCollectionControler.NextSolButton"]
+        let backSolButton = app.buttons["PhotosCollectionViewController.PreviousSolButton"]
+
+        XCTAssert(nextSolButton.exists)
+        XCTAssert(backSolButton.exists)
+
+
+        nextSolButton.tap()
+        backSolButton.tap()
+    }
+
+    func testBackButton() {
+
+        let roverCell = app.collectionViews.cells.element(boundBy: 0)
+        let backButton = app.buttons["Sol 14"]
+
+        roverCell.tap()
+        backButton.tap()
+
+        XCTAssert(app.navigationBars["Sol 14"].exists)
+        XCTAssertTrue(backButton.exists)
+
+    }
+
+    func testScollingFeatures() {
+
+        let roverCell = app.collectionViews
+
+        roverCell.element.swipeUp()
+        roverCell.element.swipeDown()
     }
 
 
