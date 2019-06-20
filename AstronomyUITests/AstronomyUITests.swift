bc58205dc692026406478d7c61a994f1f76cfb7b
Mitchell Budge <mitchellbudge@gmail.com>
2019-06-20 15:16:29 -0600

    Add UI tests for saving photos, viewing another sol, tapping a photo, and returning to the collection view

--- AstronomyUITests/AstronomyUITests.swift
+++ AstronomyUITests/AstronomyUITests.swift
@@ -17,6 +17,42 @@ class AstronomyUITests: XCTestCase {
         app.launchArguments = ["UITesting"]
     }
 
+    func testSavePhoto() {
+        let app = XCUIApplication()
+        let photos = app.collectionViews
+        let selectedCell1 = photos.cells.element(boundBy: 0)
+        selectedCell1.tap()
+        let saveButton = app.buttons["PhotoDetailViewController.SaveButton"]
+        saveButton.tap()
+        XCTAssert(app.alerts["Photo Saved!"].exists)
+        app.alerts["Photo Saved!"].buttons["Okay"].tap()
+    }
    
+    func testViewAnotherSol() {
+        let app = XCUIApplication()
+        let nextSolButton = app.buttons["PhotosCollectionViewController.NextSolButton"]
+        nextSolButton.tap()
+        XCTAssert(app.navigationBars["Sol 2"].exists)
+    }
+    
+    func testTapPhoto() {
+        let app = XCUIApplication()
+        let photos = app.collectionViews
+        let selectedCell2 = photos.cells.element(boundBy: 1)
+        selectedCell2.tap()
+        XCTAssert(app.staticTexts["Mast Camera"].exists)
+    }
+    
+    func testReturnToPhotos() {
+        let app = XCUIApplication()
+        let photos = app.collectionViews
+        let selectedCell3 = photos.cells.element(boundBy: 2)
+        selectedCell3.tap()
+        let backButton = app.buttons["Sol 1"]
+        backButton.tap()
+        XCTAssert(app.navigationBars["Sol 1"].exists)
+    }
 
+    
+    
 }
