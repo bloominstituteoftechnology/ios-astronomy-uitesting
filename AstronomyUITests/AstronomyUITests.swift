7ff83559412182a61a496730abaf8a02aeab12f9
hectorsvill <hectorstevenvillasano@gmail.com>
2019-06-20 20:29:33 -0700

    refactor to computed properties

--- AstronomyUITests/AstronomyUITests.swift
+++ AstronomyUITests/AstronomyUITests.swift
@@ -11,8 +11,6 @@ import XCTest
 class AstronomyUITests: XCTestCase {
 
     override func setUp() {
-        continueAfterFailure = false
-		XCUIApplication().launch()
 		let app = XCUIApplication()
 		app.launchArguments = ["UITesting"]
 		app.launch()
@@ -22,31 +20,25 @@ class AstronomyUITests: XCTestCase {
 		return XCUIApplication()
 	}
 	
-	
-	
-	
 	func testNextSol() {
 		let nextButton = app.navigationBars.buttons["PhotosCollectionViewController.NextSolButton"]
-	
-		let solLabel = app.otherElements["Sol 15"]
-		
+		let solLabel = title["Sol 15"]
+
 		XCTAssert(solLabel.waitForExistence(timeout: 2))
 		nextButton.tap()
 		
-		let newSolLabel = app.otherElements["Sol 16"]
+		let newSolLabel = title["Sol 16"]
 		XCTAssert(newSolLabel.waitForExistence(timeout: 2))
-	
-	
 	}
 	
 	func testPreviouseSol() {
 		let nextButton = app.navigationBars.buttons["PhotosCollectionViewController.NextSolButton"]
-		let solLabel = app.otherElements["Sol 15"]
+		let solLabel = title["Sol 15"]
 		
 		XCTAssert(solLabel.waitForExistence(timeout: 2))
 		nextButton.tap()
 		
-		let newSolLabel = app.otherElements["Sol 16"]
+		let newSolLabel = title["Sol 16"]
 		XCTAssert(newSolLabel.waitForExistence(timeout: 2))
 		
 		let previousButton = app.navigationBars.buttons["PhotosCollectionViewController.PreviousSolButton"]
@@ -56,23 +48,29 @@ class AstronomyUITests: XCTestCase {
 	}
 	
 	func testPhotoCellId0() {
-		let cell0 = app.collectionViews["MyPhotosCollection"].cells["PhotoCell id: 0"]
 		cell0.tap()
-		XCTAssert(app.otherElements["725"].waitForExistence(timeout: 2))
-		
+		XCTAssert(title["725"].waitForExistence(timeout: 2))
 	}
 	
 	func testSwipeDownSelectCell22(){
 		app.swipeUp()
 		app.swipeUp()
-		XCTAssert(app.otherElements["Sol 15"].waitForExistence(timeout: 1))
+		XCTAssert(title["Sol 15"].waitForExistence(timeout: 1))
 		
-		//PhotoCell id: 22
-		let cell22 = app.collectionViews["MyPhotosCollection"].cells["PhotoCell id: 22"]
 		cell22.tap()
-		XCTAssert(app.otherElements["58811"].waitForExistence(timeout: 1))
+		XCTAssert(title["58811"].waitForExistence(timeout: 1))
 		
 	}
 	
+	var title: XCUIElementQuery {
+		return app.otherElements
+	}
 	
+	var cell22: XCUIElement {
+		return self.app.collectionViews["MyPhotosCollection"].cells["PhotoCell id: 22"]
+	}
+	
+	var cell0: XCUIElement {
+		return app.collectionViews["MyPhotosCollection"].cells["PhotoCell id: 0"]
+	}
 }
