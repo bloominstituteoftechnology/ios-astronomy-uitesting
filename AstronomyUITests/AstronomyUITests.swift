66381cceab95f778645ce531e99d18ee0d33d06b
seanacres <6416690+seanacres@users.noreply.github.com>
2019-08-15 16:43:22 -0400

    Add astronomy ui tests and two starter tests

---
+++ AstronomyUITests/AstronomyUITests.swift
@@ -0,0 +1,71 @@
+//
+//  AstronomyUITests.swift
+//  AstronomyUITests
+//
+//  Created by Sean Acres on 8/15/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import XCTest
+
+class AstronomyUITests: XCTestCase {
+
+    var app: XCUIApplication!
+    
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        app = XCUIApplication()
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        app.launch()
+        app.launchArguments = ["UITesting"]
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    var savePhotoButton: XCUIElement {
+        let button = app.buttons["PhotoDetailViewController.SaveButton"]
+        XCTAssertTrue(button.exists)
+        return button
+    }
+    
+    var nextSolButton: XCUIElement {
+        let button = app.buttons["PhotosCollectionViewController.NextSolButton"]
+        XCTAssertTrue(button.exists)
+        return button
+    }
+    
+    var previousSolButton: XCUIElement {
+        let button = app.buttons["PhotosCollectionViewController.PreviousSolButton"]
+        XCTAssertTrue(button.exists)
+        return button
+    }
+    
+    func testSavePhoto() {
+        savePhotoButton.tap()
+        
+    }
+    
+    func testViewNextAndPreviousSol() {
+        nextSolButton.tap()
+    }
+    
+    func testViewPreviousSol() {
+        nextSolButton.tap()
+        previousSolButton.tap()
+    }
+    
+
+}
