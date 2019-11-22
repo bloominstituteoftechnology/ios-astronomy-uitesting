9c22b5e4b9ed22e8e812bcbc1347df83308a199d
John Kouris <jdkouris@gmail.com>
2019-11-21 18:56:22 -0800

    Added five more UI tests and created an extension on XCUIApplication for testing static text

--- AstronomyUITests/AstronomyUITests.swift
+++ AstronomyUITests/AstronomyUITests.swift
@@ -16,25 +16,48 @@ class AstronomyUITests: XCTestCase {
     }
 
     override func setUp() {
-        let app = XCUIApplication()
         app.launchArguments = ["UITesting"]
         app.launch()
-        // In UI tests it is usually best to stop immediately when a failure occurs.
+        
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
-
-    func testForwardNavigation() {
+    
+    func testCollectionViewLoads() {
+        XCTAssert(app.collectionViews.element.exists)
+    }
+    
+    func testForwardBackwardNavigation() {
         app.buttons["PhotosCollectionViewController.NextSolButton"].tap()
-        XCTAssert(app.navigationBars["Sol 16"].exists)
-    }
-    
-    func testBackwardNavigation() {
+        XCTAssert(app.navigationBars["Sol 2"].exists)
         app.buttons["PhotosCollectionViewController.PreviousSolButton"].tap()
-        XCTAssert(app.navigationBars["Sol 14"].exists)
+        XCTAssert(app.navigationBars["Sol 1"].exists)
     }
     
+    func testSavePhotoPopUp() {
+        app.collectionViews.children(matching: .cell).element(boundBy: 3).children(matching: .other).element.tap()
+        XCTAssert(app.buttons["Save to Photo Library"].exists)
+        app.buttons["PhotoDetailViewController.SaveButton"].tap()
+        XCTAssert(app.alerts["Photo Saved!"].exists)
+    }
     
+    func testPictureScrolling() {
+        app.collectionViews.element.swipeUp()
+        XCTAssert(app.collectionViews.children(matching: .cell).element(boundBy: 7).exists)
+        app.collectionViews.element.swipeDown()
+        XCTAssert(app.collectionViews.children(matching: .cell).element(boundBy: 0).exists)
+    }
     
+    func testSelectionAndViewsAppears() {
+        app.collectionViews.children(matching: .cell).element(boundBy: 0).children(matching: .other).element.tap()
+        XCTAssert(app.images["PhotoDetailViewController.ImageView"].exists)
+        app.assertTextExists("Camera:")
+        app.assertTextExists("Mast Camera")
+    }
+    
+}
+
+extension XCUIApplication {
+    func assertTextExists(_ staticText: String) {
+        XCTAssert(staticTexts["\(staticText)"].exists)
+    }
 }
