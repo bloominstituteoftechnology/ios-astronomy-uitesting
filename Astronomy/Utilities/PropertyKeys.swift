79021c60db682369af8fb9bc84a54b77d8556421
Jon Bash <jonbash@protonmail.com>
2019-12-12 12:33:57 -0800

    add propertyKeys string extension

---
+++ Astronomy/Utilities/PropertyKeys.swift
@@ -0,0 +1,51 @@
+//
+//  PropertyKeys.swift
+//  Astronomy
+//
+//  Created by Jon Bash on 2019-12-12.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    struct Keys {
+        static let uiTesting = (
+            collectionVC: (
+                photoCell: "PhotoCell",
+                button: (
+                    prevSol: "prevSol",
+                    nextSol: "nextSol"
+                ),
+                label: (
+                    prevSol: "",
+                    nextSol: ""
+                )
+            ),
+            detailVC: (
+                saveToPhotoLibraryButton: (
+                    text: "Save to Photo Library",
+                    id: "saveToPhotoLibrary"
+                ),
+                titleID: "",
+                descriptionID: "",
+                cameraLabelID: ""
+            ),
+            sol14: (
+                title: "Sol 14",
+                photo: (
+                    first: (
+                        title: "Sol 14 - Photo 726",
+                        description: "Taken by 5 on 8/19/12, 5:00 PM (Sol 14)",
+                        camera: "Camera: Front Hazard Avoidance Camera"
+                    ),
+                    last: (
+                        title: "Sol 14 - Photo 49158",
+                        description: "Taken by 5 on 8/19/12, 5:00 PM (Sol 14)",
+                        camera: "Camera: Navigation Camera"
+                    )
+                )
+            )
+        )
+    }
+}
