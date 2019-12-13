3235977b74e5853c2066944b1f46c75f3b52e613
Jerry Haaser <jerry.haaser@hotmail.com>
2019-12-12 18:51:22 -0600

    modified ui tests

--- AstronomyUITests/AstronomyUITests.swift
+++ AstronomyUITests/AstronomyUITests.swift
@@ -10,37 +10,36 @@ import XCTest
 
 class AstronomyUITests: XCTestCase {
     
-    var app: XCUIApplication!
+    let app: XCUIApplication = XCUIApplication()
 
     override func setUp() {
-        app = XCUIApplication()
-        app.launchArguments = ["UITesting"]
         continueAfterFailure = false
+        app.launchArguments = ["UITesting"]
+        
         app.launch()
     }
-    
     func testGoToPreviousSol() {
-        app = XCUIApplication()
+      
         let previousSolButton = app.buttons["PhotosCollectionView.previousSolButton"]
         previousSolButton.tap()
         XCTAssert(app.navigationBars["Sol 14"].exists)
     }
     
     func testGoToNextSol() {
-        app = XCUIApplication()
+        
         let nextSolButton = app.buttons["PhotosCollectionView.nextSolButton"]
         nextSolButton.tap()
         XCTAssert(app.navigationBars["Sol 16"].exists)
     }
     
     func testImageLoad() {
-        app = XCUIApplication()
+       
         app.collectionViews.children(matching: .cell).element(boundBy: 0).children(matching: .other).element.tap()
         XCTAssert(app.buttons["PhotoDetailView.saveButton"].exists)
     }
     
     func testImageSave() {
-        app = XCUIApplication()
+        
         app.collectionViews.children(matching: .cell).element(boundBy: 6).children(matching: .other).element.tap()
         app.buttons["PhotoDetailView.saveButton"].tap()
         XCTAssert(app.alerts["Photo Saved!"].exists)
