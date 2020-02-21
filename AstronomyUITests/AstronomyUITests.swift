481ab67553c16af18526b5121d34bea6cae6024a
Jorge Alvarez <jorgelalvarez52@gmail.com>
2020-02-20 20:42:23 -0800

    start

---
+++ AstronomyUITests/AstronomyUITests.swift
@@ -0,0 +1,23 @@
+//
+//  AstronomyUITests.swift
+//  AstronomyUITests
+//
+//  Created by Jorge Alvarez on 2/20/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import XCTest
+
+class AstronomyUITests: XCTestCase {
+
+    override func setUp() {
+        continueAfterFailure = false
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func test() {
+        
+    }
+    
+}
