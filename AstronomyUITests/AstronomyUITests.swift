2a81d81623c97952ec7d69c1adc963a5d2d5db2e
knopptimism <56057392+knopptimism@users.noreply.github.com>
2020-04-16 15:28:41 -0700

    Added properties for tests.

--- AstronomyUITests/AstronomyUITests.swift
+++ AstronomyUITests/AstronomyUITests.swift
@@ -9,29 +9,26 @@
 import XCTest
 
 class AstronomyUITests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    
+    //MARK: - PROPERTIES
+    
+    let app = XCUIApplication()
+    
+    private var SaveButton: XCUIElement {
+        return app.staticTexts["PhotoDetailViewController.SaveButton"]
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+       
+    private var detailLabel: XCUIElement {
+        return app.staticTexts["PhotoDetailViewController.SolDetailLabel"]
     }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
-
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    private var detailImageView: XCUIElement {
+        return app.images["PhotoDetailViewController.ImageView"]
     }
-
+    private var solDescription: XCUIElement?
+    
+    
+    
+    
     func testLaunchPerformance() throws {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
             // This measures how long it takes to launch your application.
