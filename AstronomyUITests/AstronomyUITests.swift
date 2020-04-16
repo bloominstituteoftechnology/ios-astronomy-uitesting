6d46806398dee657a2008c6dec0f2558372684fe
Mark Gerrior <github@gerrior.com>
2020-04-16 16:47:29 -0700

    Better understanding of UITesting

--- AstronomyUITests/AstronomyUITests.swift
+++ AstronomyUITests/AstronomyUITests.swift
@@ -20,7 +20,8 @@ class AstronomyUITests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
-
+        app.launchArguments.append("UITesting")
+        
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
 
