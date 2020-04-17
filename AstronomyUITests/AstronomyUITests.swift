1317ec1d6bb1999fd448ce46c429b63e9de3f12f
Shawn Gee <shawngee@gmail.com>
2020-04-16 23:59:32 -0400

    Spacing cleanup

--- AstronomyUITests/AstronomyUITests.swift
+++ AstronomyUITests/AstronomyUITests.swift
@@ -112,16 +112,14 @@ class AstronomyUITests: XCTestCase {
         XCTAssertEqual(navBarTitleLabel, "Sol 16")
     }
 
-    
-    
-    //    func testLaunchPerformance() throws {
-    //        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
-    //            // This measures how long it takes to launch your application.
-    //            measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
-    //                XCUIApplication().launch()
-    //            }
-    //        }
-    //    }
+    func testLaunchPerformance() throws {
+        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
+            // This measures how long it takes to launch your application.
+            measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
+                XCUIApplication().launch()
+            }
+        }
+    }
     
     // MARK: - Helper Functions
     
@@ -155,5 +153,4 @@ class AstronomyUITests: XCTestCase {
         }
         return nil
     }
-
 }
