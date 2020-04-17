3642040b579369aae919233e32686d516107fac6
Denis Cedeno <denis.cedeno@gmail.com>
2020-04-17 01:58:10 -0400

    4 assertion test succeeded

--- AstronomyUITests/AstronomyUITests.swift
+++ AstronomyUITests/AstronomyUITests.swift
@@ -9,60 +9,50 @@
 import XCTest
 
 class AstronomyUITests: XCTestCase {
-
+    
+    let app = XCUIApplication()
+    
     override func setUp() {
         continueAfterFailure = false
-
-          let app = XCUIApplication()
-          app.launchArguments = ["UITesting"]
-          app.launch()
+        app.launchArguments = ["UITesting"]
+        app.launch()
     }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
+    
     func testSavePhoto() {
         
-        let app = XCUIApplication()
-
         app.collectionViews
             .children(matching: .cell).element(boundBy: 1)
             .children(matching: .other).element.tap()
         app/*@START_MENU_TOKEN@*/.buttons["PhotoDetailViewController.SaveButton"]/*[[".buttons[\"Save to Photo Library\"]",".buttons[\"PhotoDetailViewController.SaveButton\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-        app.alerts["Photo Saved!"].scrollViews.otherElements.buttons["Okay"].tap()
-
         
+        XCTAssertEqual(app.alerts.element.label, "Photo Saved!")
     }
-
+    
     func testNextButtonTapped() {
-        XCUIApplication()
-            .navigationBars["Sol 15"]
-    /*@START_MENU_TOKEN@*/.buttons["PhotosCollectionViewController.NextSolButton"]/*[[".buttons[\">\"]",".buttons[\"PhotosCollectionViewController.NextSolButton\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
         
+        app.navigationBars["Sol 15"]/*@START_MENU_TOKEN@*/.buttons["PhotosCollectionViewController.NextSolButton"]/*[[".buttons[\">\"]",".buttons[\"PhotosCollectionViewController.NextSolButton\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        XCTAssertTrue(app.navigationBars["Sol 16"].exists)
     }
     
     func testPreviousButtonTapped() {
         
-        let app = XCUIApplication()
+        app.navigationBars["Sol 15"]/*@START_MENU_TOKEN@*/.buttons["PhotosCollectionViewController.NextSolButton"]/*[[".buttons[\">\"]",".buttons[\"PhotosCollectionViewController.NextSolButton\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        app.navigationBars["Sol 16"]/*@START_MENU_TOKEN@*/.buttons["PhotosCollectionViewController.PreviousSolButton"]/*[[".buttons[\"<\"]",".buttons[\"PhotosCollectionViewController.PreviousSolButton\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
         
-        app.navigationBars["Sol 15"]
-    /*@START_MENU_TOKEN@*/.buttons["PhotosCollectionViewController.NextSolButton"]/*[[".buttons[\">\"]",".buttons[\"PhotosCollectionViewController.NextSolButton\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-        app.navigationBars["Sol 16"]
-/*@START_MENU_TOKEN@*/.buttons["PhotosCollectionViewController.PreviousSolButton"]/*[[".buttons[\"<\"]",".buttons[\"PhotosCollectionViewController.PreviousSolButton\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-       
+        XCTAssertTrue(app.navigationBars["Sol 15"].exists)
+        XCTAssertFalse(app.navigationBars["Sol 16"].exists)
         
     }
     
     func testNextButtonViewPhotoAndBackToCollectionView() {
-        let app = XCUIApplication()
- XCUIApplication()
-            .navigationBars["Sol 15"]
-    /*@START_MENU_TOKEN@*/.buttons["PhotosCollectionViewController.NextSolButton"]/*[[".buttons[\">\"]",".buttons[\"PhotosCollectionViewController.NextSolButton\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        
+        app.navigationBars["Sol 15"]/*@START_MENU_TOKEN@*/.buttons["PhotosCollectionViewController.NextSolButton"]/*[[".buttons[\">\"]",".buttons[\"PhotosCollectionViewController.NextSolButton\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
         app.collectionViews
-        .children(matching: .cell).element(boundBy: 1)
-        .children(matching: .other).element.tap()
+            .children(matching: .cell).element(boundBy: 1)
+            .children(matching: .other).element.tap()
         app.navigationBars["Title"].buttons["Sol 16"].tap()
+        
+        XCTAssertTrue(app.navigationBars["Sol 16"].exists)
         
     }
 }
