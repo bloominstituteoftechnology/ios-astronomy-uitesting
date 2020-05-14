5bae14f0d52721a05fa76077e7e8fc8d22ed3eef
Chris Dobek <dobek.christopher@gmail.com>
2020-05-14 16:47:58 -0400

    MVP

--- AstronomyUITests/AstronomyUITests.swift
+++ AstronomyUITests/AstronomyUITests.swift
@@ -37,16 +37,26 @@ class AstronomyUITests: XCTestCase {
     }
     
     func testTapCell() {
-        app.collectionViews.children(matching: .cell).element(boundBy: 0).otherElements.children(matching: .image).element.tap()
+        app.collectionViews.children(matching: .cell).element(boundBy: 0).images["PhotosCollectionViewController.PhotoCell"].tap()
+        
     }
     
     func testSavePhoto() {
         
-        app.collectionViews.children(matching: .cell).element(boundBy: 0).otherElements.children(matching: .image).element.tap()
+        app.collectionViews.children(matching: .cell).element(boundBy: 0).images["PhotosCollectionViewController.PhotoCell"].tap()
+        
+        app.children(matching: .window).element(boundBy: 0).children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.tap()
+        
+        app/*@START_MENU_TOKEN@*/.staticTexts["PhotoDetailViewController.DetailLabel"]/*[[".staticTexts[\"Taken by 5 on 8\/6\/12, 8:00 PM (Sol 1)\"]",".staticTexts[\"PhotoDetailViewController.DetailLabel\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        
+        app/*@START_MENU_TOKEN@*/.staticTexts["PhotoDetailViewController.Camera"]/*[[".staticTexts[\"Camera:\"]",".staticTexts[\"PhotoDetailViewController.Camera\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        
+        app/*@START_MENU_TOKEN@*/.staticTexts["PhotoDetailViewController.CameraLabel"]/*[[".staticTexts[\"Mast Camera\"]",".staticTexts[\"PhotoDetailViewController.CameraLabel\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        
         app/*@START_MENU_TOKEN@*/.staticTexts["Save to Photo Library"]/*[[".buttons[\"Save to Photo Library\"].staticTexts[\"Save to Photo Library\"]",".buttons[\"PhotoDetailViewController.SaveButton\"].staticTexts[\"Save to Photo Library\"]",".staticTexts[\"Save to Photo Library\"]"],[[[-1,2],[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        
         app.alerts["Photo Saved!"].scrollViews.otherElements.buttons["Okay"].tap()
         app.navigationBars["Title"].buttons["Sol 1"].tap()
-        
     }
 
     func testLaunchPerformance() throws {
