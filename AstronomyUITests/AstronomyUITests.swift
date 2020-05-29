6eb2772a816f06232be3cd4e18497505968dede2
Matt Martindale <martindalematt18@yahoo.com>
2020-05-28 18:48:51 -0600

    save photo to photo library test successful

--- AstronomyUITests/AstronomyUITests.swift
+++ AstronomyUITests/AstronomyUITests.swift
@@ -9,6 +9,10 @@
 import XCTest
 
 class AstronomyUITests: XCTestCase {
+    
+    var app: XCUIApplication {
+        return XCUIApplication()
+    }
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -19,6 +23,18 @@ class AstronomyUITests: XCTestCase {
         continueAfterFailure = false
     }
     
-    
+    func testSavingPhoto() {
+        app.launch()
+        app.collectionViews.children(matching: .cell).element(boundBy: 0).children(matching: .other).element.tap()
+        // Test if detail label shows correct String
+        let detailLabel = app.staticTexts["imageDetailLabel"]
+        XCTAssertTrue(detailLabel.label == "Taken by 5 on 8/6/12, 6:00 PM (Sol 1)")
+        
+        // Test if photo was saved successfully
+        app/*@START_MENU_TOKEN@*/.staticTexts["Save to Photo Library"]/*[[".buttons[\"Save to Photo Library\"].staticTexts[\"Save to Photo Library\"]",".buttons[\"PhotoDetailViewController.SaveButton\"].staticTexts[\"Save to Photo Library\"]",".staticTexts[\"Save to Photo Library\"]"],[[[-1,2],[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        let alertText = XCUIApplication().alerts["Photo Saved!"].scrollViews.otherElements.staticTexts["Photo Saved!"]
+        XCTAssertTrue(alertText.label == "Photo Saved!")
+        app.alerts["Photo Saved!"].scrollViews.otherElements.buttons["Okay"].tap()
+    }
     
 }
