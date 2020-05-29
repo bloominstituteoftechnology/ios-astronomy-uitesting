dd7630df521b5ab55b9722df7023feb006e1a712
Breena <breena_greek@live.com>
2020-05-28 22:46:33 -0400

    Working on checking if cell image and detail image are the same

--- AstronomyUITests/AstronomyUITests.swift
+++ AstronomyUITests/AstronomyUITests.swift
@@ -10,35 +10,41 @@ import XCTest
 @testable import Astronomy
 
 class AstronomyUITests: XCTestCase {
-
+    
     override func setUpWithError() throws {
         let app = XCUIApplication()
         app.launchArguments = ["UITesting"]
     }
-
+    
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
+    
     func test_picture_saved(){
         let app = XCUIApplication()
         app.launch()
-                
-//        app.collectionViews.cells.staticTexts["PhotosCollectionViewController.ImageCell"].tap()
-        app.collectionViews.children(matching: .cell).element(boundBy: 0).children(matching: .other).element.tap()
+        
+        XCUIApplication().collectionViews.children(matching: .cell).element(boundBy: 0).otherElements.containing(.image, identifier:"PhotosCollectionViewController.ImageCell").element.tap()
+        
         let saveButtonTapped = app/*@START_MENU_TOKEN@*/.staticTexts["Save to Photo Library"]/*[[".buttons[\"Save to Photo Library\"].staticTexts[\"Save to Photo Library\"]",".buttons[\"PhotoDetailViewController.SaveButton\"].staticTexts[\"Save to Photo Library\"]",".staticTexts[\"Save to Photo Library\"]"],[[[-1,2],[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
         saveButtonTapped.tap()
+        
         app.alerts["Photo Saved!"].scrollViews.otherElements.buttons["Okay"].tap()
-                
+        
         XCTAssertTrue(saveButtonTapped.isEnabled)
     }
-
-//    func testLaunchPerformance() throws {
-//        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
-//            // This measures how long it takes to launch your application.
-//            measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
-//                XCUIApplication().launch()
-//            }
-//        }
-//    }
+    
+    func test_image_same_when_selected() {
+        
+        let app = XCUIApplication()
+        app.launch()
+        
+        var expectedImage =  "PhotosCollectionViewController.ImageCell"
+        
+        var imageSelected = app.collectionViews.children(matching: .cell).element(boundBy: 0).otherElements.containing(.image, identifier:"PhotosCollectionViewController.ImageCell").element
+        imageSelected.tap()
+        
+        XCTAssertEqual(expectedImage, imageSelected.identifier)
+        
+    }
 }
