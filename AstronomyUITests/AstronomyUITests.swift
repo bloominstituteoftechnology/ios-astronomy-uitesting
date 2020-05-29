741a23b0d6243b9308a635c30d4fc7a8bac30563
Claudia Maciel <clc_80@yahoo.com>
2020-05-28 18:58:13 -0700

    Testing the user saving a  phot

--- AstronomyUITests/AstronomyUITests.swift
+++ AstronomyUITests/AstronomyUITests.swift
@@ -13,38 +13,30 @@ class AstronomyUITests: XCTestCase {
     override func setUp() {
         super.setUp()
         let app = XCUIApplication()
+        
         app.launchArguments.append("UITesting")
     }
-    
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
+    // Perform test for saving a photo
     func testExample() throws {
-        // UI tests must launch the application that they test.
+        // Click the image
         let app = XCUIApplication()
         app.launch()
-
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
-                XCUIApplication().launch()
-            }
-        }
+        let imageTapped = app.collectionViews.children(matching: .cell).element(boundBy: 2).otherElements.containing(.image, identifier:"com.astronomy.imagecell.image").element
+        imageTapped.tap()
+        
+        
+        //Is the user pushing the Save Button?
+        let saveButton = app.buttons["PhotoDetailViewController.SaveButton"]
+        XCTAssert(saveButton.isHittable)
+        
+        //Push the save button
+        saveButton.tap()
+        
+        // Did the right alert show up?
+        XCTAssertEqual(app.alerts.element.label, "Photo Saved!")
+        
+        // Tap ok on the alerts
+        app.alerts["Photo Saved!"].scrollViews.otherElements.buttons["Okay"].tap()
     }
 }
