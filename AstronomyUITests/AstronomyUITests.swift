fb6ce58587b09ec8366c9a05844696b21ae538d5
Clayton Watkins <watkinclayton17@yahoo.com>
2020-07-30 10:44:32 -0700

    Created four complete, passing UI Tests.

--- AstronomyUITests/AstronomyUITests.swift
+++ AstronomyUITests/AstronomyUITests.swift
@@ -7,37 +7,51 @@
 //
 
 import XCTest
+@testable import Astronomy
 
 class AstronomyUITests: XCTestCase {
+    var app = XCUIApplication()
+    
+    func testSavingPhotoToLibrary() {
+        try! setUpWithError()
+        app.collectionViews.children(matching: .cell).element(boundBy: 0).images["imageCell"].tap()
+        app/*@START_MENU_TOKEN@*/.staticTexts["Save to Photo Library"]/*[[".buttons[\"Save to Photo Library\"].staticTexts[\"Save to Photo Library\"]",".buttons[\"savePhotoToLibraryButton \"].staticTexts[\"Save to Photo Library\"]",".staticTexts[\"Save to Photo Library\"]"],[[[-1,2],[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        let completedAlert = app.alerts["Photo Saved!"].scrollViews.otherElements.buttons["Okay"]
+        XCTAssertEqual(completedAlert.label, "Okay")
+    }
+    
+    func testChangingSol() {
+        try! setUpWithError()
+        app.navigationBars["Sol 15"]/*@START_MENU_TOKEN@*/.buttons["PhotosCollectionViewController.PreviousSolButton"]/*[[".buttons[\"<\"]",".buttons[\"PhotosCollectionViewController.PreviousSolButton\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        let newSolTitle = app.navigationBars["Sol 14"].staticTexts["Sol 14"]
+        XCTAssertEqual(newSolTitle.label, "\(newSolTitle.label)")
+    }
+    
+    func testViewingCellDetail() {
+        try! setUpWithError()
+        app.collectionViews.children(matching: .cell).element(boundBy: 3).images["imageCell"].tap()
+        let photo = app.images["PhotoDetailViewController.ImageView"].waitForExistence(timeout: 2)
+        XCTAssert(photo)
+    }
+    
+    func testLeavingDetailView() {
+        try! setUpWithError()
+        let solStaticText = app.navigationBars["Sol 15"].staticTexts["Sol 15"]
+        solStaticText.tap()
+        app.collectionViews.children(matching: .cell).element(boundBy: 1).images["imageCell"].tap()
+        let titleNavigationBar = app.navigationBars["Title"]
+        titleNavigationBar.staticTexts["Title"].tap()
+        titleNavigationBar.buttons["Sol 15"].tap()
+        solStaticText.tap()
+        XCTAssertEqual(solStaticText.label, "Sol 15")
+    }
+    
+}
 
+extension AstronomyUITests {
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
         let app = XCUIApplication()
+        app.launchArguments = ["UITesting"]
         app.launch()
-
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
-                XCUIApplication().launch()
-            }
-        }
     }
 }
