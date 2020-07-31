52cc457593e24344493529ac9c8c9852f8db52da
kensam1 <samarakodykennedy@gmail.com>
2020-07-30 23:40:19 -0400

    UI Testing. Need to finish

---
+++ AstronomyUITests/AstronomyUITests.swift
@@ -0,0 +1,19 @@
+//
+//  AstronomyUITests.swift
+//  AstronomyUITests
+//
+//  Created by Lambda_School_Loaner_241 on 7/30/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import XCTest
+
+class AstronomyUITests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        let app = XCUIApplication()
+        app.launchArguments = ["UITesting"]
+
+    }
+}
